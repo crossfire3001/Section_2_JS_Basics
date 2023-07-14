f5aba7d467e04ee04346a641e5acc6c6057a57e1
crossfire3001 <obiwan2035@mail.ru>
2023-07-14 14:09:37 +0500

    first commit

---
+++ 32 - Objects object destructuring - ES6/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: "black",
+    bg: "red",
+  },
+};
+
+console.log(options.name);
+
+// удаление свойства
+delete options.name;
+
+console.log(options);
+
+// перебор всех свойств объекта - ключ: значение
+for (let key in options) {
+  console.log(`Свойство ${key} имеет значение ${options[key]}`);
+}
