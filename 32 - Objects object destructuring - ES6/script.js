7ad861dd92c25b921dc65fcbab9110957a99d389
crossfire3001 <obiwan2035@mail.ru>
2023-07-15 19:14:58 +0500

    enumeration of all object properties JS

--- 32 - Objects object destructuring - ES6/script.js
+++ 32 - Objects object destructuring - ES6/script.js
@@ -17,7 +17,20 @@ delete options.name;
 
 console.log(options);
 
+let counter = 0;
 // перебор всех свойств объекта - ключ: значение
 for (let key in options) {
-  console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  if (typeof options[key] === "object") {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+      counter++;
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
 }
+
+console.log(options["colors"]["border"]);
+
+console.log(counter);
